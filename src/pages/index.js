571566129b89fd786fd8e2f4521cd1be0de18205
Robert Maxwell <robertjamesmaxwell@gmail.com>
2020-06-22 21:00:19 -0500

    Added emotion css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Nola ya heard</p>
-    <Link to="/about">Read about me &rarr;</Link>
-  </>
+    <Link to="/about/">Read about me &rarr;</Link>
+  </Layout>
 );
