68369c680d8825d4093f972ca1f48600407ada52
Robert Maxwell <robertjamesmaxwell@gmail.com>
2020-06-27 11:52:04 -0500

    Add local blog files as posts with graphql

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,30 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+  return (
+    data &&
+    data.allMdx &&
+    data.allMdx.nodes.map(node => {
+      const { title, author, slug } = node.frontmatter;
+      const post = { title, author, slug, excerpt: node.excerpt };
+      console.log('post', post);
+      return post;
+    })
+  );
+};
+
+export default usePosts;
